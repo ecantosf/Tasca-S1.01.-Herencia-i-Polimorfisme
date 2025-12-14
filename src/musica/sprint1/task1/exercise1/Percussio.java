7cb58d06312e16909b5e66bc5840fd8c93b2702b
fonca <eduard.cantos@gmail.com>
2025-12-14 16:07:41 +0100

    feat(classes): add Percussio instrument implementation

--- src/musica/sprint1/task1/exercise1/Percussio.java
+++ src/musica/sprint1/task1/exercise1/Percussio.java
@@ -1,5 +1,28 @@
-package musica.sprint1.task1.exercise1;
+public class Percussio extends Instrument {
 
-public class Percussio extends Instrument{
-    //Buit provisionalment
+    //Atribut estàtic específic
+    public static String descripcio;
+
+    //Bloc Static: es carrega quan s'accedeix a la classe (percussió) per primera vegada
+    static {
+        System.out.println("BLOQ ESTÀTIC de la classe Percussió - Carregant classe...");
+        descripcio = "Instrument de percussió";
+        System.out.println("Tipus d'instrument: " + descripcio);
+    }
+
+    //Bloc d'inicialització d'instància
+    {
+        System.out.println("Bloc d'inicialització d'instància de percussió.");
+    }
+
+    //Constructor
+    public Percussio(String name, double price) {
+        super(name, price);
+    }
+
+    //Implementació del mètode abstracte
+    @Override
+    public void play() {
+        System.out.println("Està sonant un instrument de percussió");
+    }
 }
