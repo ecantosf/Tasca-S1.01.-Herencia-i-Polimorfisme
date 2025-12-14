6d9d8692e5833fa1db199ca007f85e908a53b7bb
fonca <eduard.cantos@gmail.com>
2025-12-14 16:04:41 +0100

    feat(classes): add Corda instrument implementation

--- src/musica/sprint1/task1/exercise1/Corda.java
+++ src/musica/sprint1/task1/exercise1/Corda.java
@@ -1,5 +1,27 @@
-package musica.sprint1.task1.exercise1;
+ppublic class Corda extends Instrument {
 
-public class Corda extends Instrument{
-    //Buit provisionalment
+    //Atribut estàtic específic de la classe Corda
+    public static final String TIPUS_CORDA = "Instrument de corda";
+
+    //Bloc Static: es carrega quan s'accedeix a la classe (corda) per primera vegada
+    static {
+        System.out.println("BLOQ ESTÀTIC de la classe Corda - Carregant classe...");
+        System.out.println("Tipus d'instrument: " + TIPUS_CORDA);
+    }
+
+    //Bloc d'inicialització d'instància
+    {
+        System.out.println("Bloc d'inicialització d'instància de Corda.");
+    }
+
+    //Constructor
+    public Corda(String name, double price) {
+        super(name, price);
+    }
+
+    //Implementació del mètode abstracte
+    @Override
+    public void play() {
+        System.out.println("Està sonant un instrument de corda");
+    }
 }
